0c14b328352ce9d69d5b2672893a543cc6233034
YokoMolina <geocondamolina3e@hotmail.com>
2023-11-17 17:31:55 -0500

    Update temporal.py

--- Tema 1/temporal.py
+++ Tema 1/temporal.py
@@ -1,13 +1,24 @@
+#shift+t
+#IMPORTAMOS LA LIBRERIA GYM PARA EL APRENDIZAJE POR REFUERZO
 import gym
 
 
-
-enviroment = gym.make("MountainCar-v0")
-#enviroment = gym.make("CartPole-v1")
-#enviroment = gym.make("Acrobot-v1")
+#ENVIROMENTS
+#_________________________________________________
+#LA FUNCIÓN MAKE CREA EN AMBIENTE LLAMDO EN COMILLAS 
+#enviroment = gym.make("MountainCar-v0")
+##enviroment = gym.make("CartPole-v1")
+enviroment = gym.make("Acrobot-v1")
 #enviroment = gym.make("Pendulum-v0")
+
+#RESETEAMOS EL AMBIENTE
 enviroment.reset()
 
+#SE GENERA UN BUCLE DE 2000 ITERACIONES 
 for _ in range(2000):
+    #CREAMOS EL RENDER DE EL AMBIENTE 
     enviroment.render()
+    #HACEMOS SE TOME DECISIONES ALEATORIAS DE TODAS LAS POSIBLES 
     enviroment.step(enviroment.action_space.sample())
+
+    
