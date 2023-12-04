eca088f835a262bebfd8fbb046d6cea04dc87e17
YokoMolina <geocondamolina3e@hotmail.com>
2023-12-04 18:15:19 -0500

    enviroments Box2d
    revisar la libreria atari, tiene conflictos

--- Tema 1/temporal.py
+++ Tema 1/temporal.py
@@ -8,8 +8,16 @@ import gym
 #LA FUNCIÓN MAKE CREA EN AMBIENTE LLAMDO EN COMILLAS 
 #enviroment = gym.make("MountainCar-v0")
 ##enviroment = gym.make("CartPole-v1")
-enviroment = gym.make("Acrobot-v1")
+#enviroment = gym.make("Acrobot-v1")
 #enviroment = gym.make("Pendulum-v0")
+
+#____________________________________
+#otros ambientes
+#enviroment = gym.make("BipedalWalker-v3")
+
+enviroment = gym.make("SpaceInvaders-v0")
+
+
 
 #RESETEAMOS EL AMBIENTE
 enviroment.reset()
@@ -21,4 +29,5 @@ for _ in range(2000):
     #HACEMOS SE TOME DECISIONES ALEATORIAS DE TODAS LAS POSIBLES 
     enviroment.step(enviroment.action_space.sample())
 
+enviroment.close
     
